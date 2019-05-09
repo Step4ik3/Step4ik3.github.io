fcb48f31dec0da6c17249f3dc24488b94d760a2b
Artem <stepura02@outlook.com>
2019-05-09 18:08:20 +0300

    Reload

--- Project(Tajam)/script/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("menu__list__mobile");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-    console.log("click works") ;}
